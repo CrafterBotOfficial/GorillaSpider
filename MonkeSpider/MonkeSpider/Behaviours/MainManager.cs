64d731c59bc286e9bdb7e78712e6af63e3f21da8
Crafterbot <118089496+CrafterBotOfficial@users.noreply.github.com>
2023-06-07 12:31:59 -0400

    None Networked commit

---
+++ MonkeSpider/MonkeSpider/Behaviours/MainManager.cs
@@ -0,0 +1,54 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace MonkeSpider.Behaviours
+{
+    internal class MainManager : MonoBehaviour
+    {
+        internal (bool IsLocal, Player player) OnlinePlayer => (GetComponent<PhotonView>().IsMine, GetComponent<PhotonView>().Owner);
+
+        internal class PlayerData
+        {
+            internal Transform[] BodyContraints; // 0 = Head, 1 = Left Arm, 2 = Right Arm
+            internal Vector3[] DefaultLocalPositions; // 0 = Head, 1 = Left Arm, 2 = Right Arm
+            internal GameObject LocalVRRigHead; // idk why this is even in the game, just degrading performance
+
+            internal PlayerData(Transform head, Transform leftArm, Transform rightArm, GameObject localVRRigHead)
+            {
+                BodyContraints = new Transform[3] { head, leftArm, rightArm };
+                DefaultLocalPositions = new Vector3[3] { head.localPosition, leftArm.localPosition, rightArm.localPosition };
+                LocalVRRigHead = localVRRigHead;
+            }
+        }
+
+        internal PlayerData SavedPlayerData;
+        internal bool InEffect;
+
+        private void Start()
+        {
+            SavedPlayerData = new PlayerData(transform.Find("rig/body/head"), transform.Find("rig/body/shoulder.L"), transform.Find("rig/body/shoulder.R"), GameObject.Find("Global/Local VRRig/Local Gorilla Player/rig/body/head/"));
+        }
+
+        private void Update()
+        {
+            if (Main.Instance.enabled && Main.Instance.InModded)
+                if (!InEffect)
+                    SetOffset(true);
+                else;
+            else if (InEffect)
+                SetOffset(false);
+        }
+
+        private void SetOffset(bool Activity)
+        {
+            InEffect = Activity;
+            for (int i = 0; i < SavedPlayerData.BodyContraints.Length; i++)
+                if (SavedPlayerData.BodyContraints[i].name == "head")
+                    SavedPlayerData.BodyContraints[i].localPosition = Activity ? SavedPlayerData.BodyContraints[i].localPosition + new Vector3(0, -0.1f, .15f) : SavedPlayerData.DefaultLocalPositions[i];
+                else
+                    SavedPlayerData.BodyContraints[i].localPosition = Activity ? (SavedPlayerData.BodyContraints[i].localPosition - (Vector3.up * 0.25f + Vector3.forward * -0.1f)) : SavedPlayerData.DefaultLocalPositions[i];
+            SavedPlayerData.LocalVRRigHead.SetActive(!Activity);
+        }
+    }
+}
---
+++ MonkeSpider/MonkeSpider/Main.cs
@@ -0,0 +1,56 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+using Utilla;
+
+namespace MonkeSpider
+{
+    [BepInPlugin(GUID, NAME, VERSION), BepInDependency("org.legoandmars.gorillatag.utilla")]
+    [ModdedGamemode, System.ComponentModel.Description("HauntedModMenu")]
+    internal class Main : BaseUnityPlugin
+    {
+        internal const string
+            GUID = "crafterbot.somedumbmonkegame.monkespider",
+            NAME = "MonkeSpider",
+            VERSION = "1.0.0";
+        internal static Main Instance;
+
+        internal ManualLogSource manualLogSource => Logger;
+        internal bool InModded;
+
+        internal Main()
+        {
+            Instance = this;
+
+            new HarmonyLib.Harmony(GUID).PatchAll();
+        }
+
+#if DEBUG
+        private void OnGUI()
+        {
+            if (!InModded)
+                GUI.Window(50, new Rect(500, 50, 200, 45), Win, "Spider monke debug");
+            void Win(int WindowID)
+            {
+                GorillaNetworking.GorillaComputer.instance.currentGameMode = "MODDED_CASUAL";
+                if (GUILayout.Button("Join Modded"))
+                    Utilla.Utils.RoomUtils.JoinPrivateLobby("crafterbot");
+            }
+        }
+#endif
+
+        #region Utilla callbacks
+        [ModdedGamemodeJoin]
+        private void OnModdedGamemodeJoin() =>
+            InModded = true;
+        [ModdedGamemodeLeave]
+        private void OnModdedGamemodeLeave() =>
+            InModded = false;
+        #endregion
+
+        public void OnEnable() =>
+            manualLogSource.LogInfo("Enabled!");
+        public void OnDisable() =>
+            manualLogSource.LogInfo("Disabled!");
+    }
+}
